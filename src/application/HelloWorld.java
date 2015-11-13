8df083eb7613b146da41135a2239aaf5a867b91f
dmedv <dmedv@jhu.edu>
2015-11-13 16:36:34 -0500

    First commit

---
+++ src/application/HelloWorld.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class HelloWorld extends HttpServlet {
+ 
+  private String username;
+
+  @Override
+  public void init(ServletConfig cfg) throws ServletException
+  {
+	  
+      username = cfg.getServletContext().getInitParameter("username");
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+  {
+      response.setContentType("text/html");
+
+      PrintWriter out = response.getWriter();
+      out.println("<h1>Hello, " + username + "</h1>");
+  }
+
+}
