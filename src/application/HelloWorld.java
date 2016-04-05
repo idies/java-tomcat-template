8efdf85f8c1a64b7eb005bf49a6b97ac15318b18
Dmitry Medvedev <dmedv@jhu.edu>
2016-04-05 16:41:44 -0400

    Added log4j2 examples

--- src/application/HelloWorld.java
+++ src/application/HelloWorld.java
@@ -3,21 +3,30 @@ package application;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HelloWorld extends HttpServlet {
 
+  private static final Logger logger = LogManager.getLogger("Foo");
   private String username;
-
+  
   @Override
   public void init(ServletConfig cfg) throws ServletException
   {
+	  logger.info("Initializing servlet...");
       username = cfg.getServletContext().getInitParameter("username");
+      logger.error("Oops! An error has occured!");
+      logger.info("Never mind, it's OK.");
   }
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
   {
+	  // This message will not appear in the log by default, 
+	  // because the Root logger level is set to "info".
+	  logger.debug("GET request received.");
+	  
       response.setContentType("text/html");
 
       PrintWriter out = response.getWriter();
