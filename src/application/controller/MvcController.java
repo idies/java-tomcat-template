9d625f9dde5104926ec26c1bd73dfa6f58cb67eb
dmedv <dmedv@jhu.edu>
2016-05-21 22:23:39 -0400

    Change RestController annotation to Controller

--- src/application/controller/MvcController.java
+++ src/application/controller/MvcController.java
@@ -5,11 +5,11 @@ import javax.servlet.ServletContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class MvcController {
     
     private static final Logger logger = LogManager.getLogger("Foo");
